231729ba340fc8c93f8688180d78ce0f838d7f19
Justinxiaohao <1418661311@qq.com>
2025-05-28 19:41:23 +0800

    修复错误

--- spring-ai/src/main/java/cn/sdtbu/edu/config/CorsConfig.java
+++ spring-ai/src/main/java/cn/sdtbu/edu/config/CorsConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author Wyh
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8081","http://localhost:5500","http://127.0.0.1:8081") // 允许前端地址
+                .allowedOrigins("http://localhost:5173", "http://localhost:8081","http://localhost:5500","http://127.0.0.1:8081,http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
