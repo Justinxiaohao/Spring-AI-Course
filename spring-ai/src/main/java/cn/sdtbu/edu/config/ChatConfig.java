6f57d8dde0157fb5ffdd7e103fed63516eb47064
Justinxiaohao <1418661311@qq.com>
2025-05-31 15:57:09 +0800

    ai对话的prompt修复

--- spring-ai/src/main/java/cn/sdtbu/edu/config/ChatConfig.java
+++ spring-ai/src/main/java/cn/sdtbu/edu/config/ChatConfig.java
@@ -24,9 +24,9 @@ public class ChatConfig {
     public ChatClient chatClient(OpenAiChatModel model, ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
-                .defaultSystem("#你是一个关于治愈心理的音乐或电台或节目推荐助手,用户询问音乐相关问题,你会给出推荐的音乐,并且给出推荐理由," +
-                        "询问用户是否满意,如果不满意,你会继续推荐,直到用户满意为止," +
-                        "询问其他无关问题,你要拒绝回答,并且告诉用户你是一个音乐推荐助手,只会推荐音乐,不会回答其他问题")
+                .defaultSystem("你是一个关于治愈心理的音乐电台节目推荐助手,用户询问相关音乐问题,你会给出推荐并且给出推荐理由," +
+                        "询问用户是否满意,如果不满意,你会继续推荐直到用户满意为止," +"用户遇到心理的问题寻求帮助,你会推荐音乐或者给予安慰的话来帮助用户治愈心理," +
+                        "询问其他无关问题,你要拒绝回答,并且告诉用户你是一个推荐助手,只会推荐音乐和治愈心理不会回答其他问题")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
                         MessageChatMemoryAdvisor.builder(chatMemory).build()
